d6eeec720d16dc83f386335129d7780186c37f21
Juan <jouglardjuan@gmail.com>
2024-07-18 16:31:55 -0300

    save vector stores on s3 bucket

--- vector_database.py
+++ vector_database.py
@@ -2,19 +2,40 @@ from langchain.docstore.document import Document as LangchainDocument
 from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
+import boto3
 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY_ID = os.getenv("AWS_SECRET_KEY_ID")
+ENDPOINT = os.getenv("S3_ENDPOINT")
+BUCKET_NAME = "vectorstores"
 
 class VectorDatabase:
-    def __init__(self, documents):
-        self.documents = documents
-        self.RAW_KNOWLEDGE_BASE = [
-            LangchainDocument(page_content=doc["content"], metadata=doc["metadata"]) for doc in documents
-        ]
+    def __init__(self):
+        self.s3_client = boto3.resource('s3',
+                                      endpoint_url=ENDPOINT,
+                                      aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                      aws_secret_access_key=AWS_SECRET_KEY_ID,
+                                      aws_session_token=None,
+                                      config=boto3.session.Config(signature_version='s3v4'),
+                                      verify=False
+                         ).Bucket(BUCKET_NAME)
 
-    def create_store(self, docs_processed: LanchainDocument[], embedding_model: HuggingFaceEmbeddings, distance: DistanceStrategy = DistanceStrategy.COSINE):
-        self.vector_store = FAISS(docs_processed, embedding_model, distance)
+    def create_store(self, chat_id:String, docs_processed: LangchainDocument[], embedding_model: HuggingFaceEmbeddings, distance: DistanceStrategy = DistanceStrategy.COSINE):
+        if self.vector_store := check_if_file_exists(chat_id):
+            print("VECTOR STORE EXISTS")
+        else:
+            self.vector_store = FAISS(docs_processed, embedding_model, distance)
 
     def similarity_search(query: String, top_n: int):
         return self.vector_store.similarity_search(query)
+
+
+    def check_if_file_exists(chat_id: String):
+
+        return self.s3_client_bucket.download_file(Filename=f"{chat_id}.pkl", Key=f"{chat_id}.pkl")
+
+    def save_file_to_bucket(self, chat_id):
+        content = self.vector_store.serializer_to_bytes()
+        return self.s3_client_bucket.put_object(Key=f"{chat_id}.pkl", Body=content)
 
 
