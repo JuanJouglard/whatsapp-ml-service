2ab94f9868cd4fad7927e6fd0e29be0e3b05e650
root <jouglardjuan@gmail.com>
2024-08-15 12:49:04 +0000

    api setup to handle file

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class ChatData(BaseModel):
+    file_id: str
 
+@app.middleware("http")
+async def log_stuff(request: Request, call_next):
+    print(f"{request.method} {request.url}")
+    print(f"{await request.body()}")
+    response = await call_next(request)
+    print(response)
+    print(response.status_code)
+    return response
 
 @app.get("/chats")
-def query_chats(query: String):
+def query_chats(query: str):
+    print(f"query: {query}")
     return {"Body": "Query chats"}
+
+@app.post("/chats")
+def parse_chat(chat: ChatData):
+    print(f"file id: {chat}")
+    return {"Body": "Parse chats"}
--- vector_database.py
+++ vector_database.py
@@ -3,6 +3,7 @@ from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
 import boto3
+import os
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY_ID = os.getenv("AWS_SECRET_KEY_ID")
@@ -26,12 +27,11 @@ class VectorDatabase:
         else:
             self.vector_store = FAISS(docs_processed, embedding_model, distance)
 
-    def similarity_search(query: String, top_n: int):
+    def similarity_search(self, query: String, top_n: int):
         return self.vector_store.similarity_search(query)
 
 
-    def check_if_file_exists(chat_id: String):
-
+    def check_if_file_exists(self, chat_id: String):
         return self.s3_client_bucket.download_file(Filename=f"{chat_id}.pkl", Key=f"{chat_id}.pkl")
 
     def save_file_to_bucket(self, chat_id):
